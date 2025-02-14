e2155adb399c4b55471dd436ccd1a281114a3ced
Gavin Walsh <gavinwalshwork@gmail.com>
2025-02-14 12:45:06 -0800

    updating header

--- my-app/src/app/dashboard/plan/page.tsx
+++ my-app/src/app/dashboard/plan/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Header } from "@/components/header";
 import Calendar from "@/components/calendar";
 
 export default function Plan() {
   return (
-    <div className="p-6">
+    <div className="p-6 flex flex-col gap-6">
+      <Header className="border-b pb-2" />
       <Calendar />
     </div>
   );
--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -3,12 +3,13 @@ import { Button } from "./button";
 import { CirclePlus } from "lucide-react";
 
 interface HeaderProps {
-  heading: string;
+  heading?: string;
+  className?: string;
 }
 
-export const Header = ({ heading }: HeaderProps) => {
+export const Header = ({ heading, className }: HeaderProps) => {
   return (
-    <header className="flex items-center justify-between">
+    <header className={`flex items-center justify-between ${className}`}>
       <h1 className="text-3xl">{heading}</h1>
       <Button text="Create a post" icon={CirclePlus} />
     </header>
