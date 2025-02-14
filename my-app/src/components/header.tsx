198ec088482f77487cebdbbd129227eb6ee715fa
Gavin Walsh <gavinwalshwork@gmail.com>
2025-02-13 23:42:02 -0800

    updates

--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -3,13 +3,13 @@ import { Button } from "./button";
 import { CirclePlus } from "lucide-react";
 
 interface HeaderProps {
-  //   heading: string;
+  heading: string;
 }
 
-export const Header = ({}: HeaderProps) => {
+export const Header = ({heading}: HeaderProps) => {
   return (
     <header className="flex items-center justify-between p-6">
-      <h1 className="text-3xl">Welcome, User!</h1>
+      <h1 className="text-3xl">{heading}</h1>
       <Button text="Create a post" icon={CirclePlus} />
     </header>
   );
